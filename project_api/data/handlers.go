d3fbe0cc861eef175c32c0e3d511fb8a404d554b
andreach36 <andrea.haro36@gmail.com>
2024-11-21 23:55:02 -0500

    fixed filter and order data function

--- project_api/data/handlers.go
+++ project_api/data/handlers.go
@@ -3,7 +3,7 @@ package data
 import (
 	"net/http"
 	"net/url"
-	"strings"
+
 	"trucode3-challenge-final/project_api/database"
 	"trucode3-challenge-final/project_api/models"
 
@@ -42,162 +42,99 @@ func ShowAllData(c *gin.Context) {
 	})
 }
 
-// filtrar y ordenar según un campo especifico
-func FilterDataByEducation(c *gin.Context) {
-	FilterAndOrderData("education", c)
-}
+func FilterAndOrderData(c *gin.Context) {
+	// Recoger los parámetros de la consulta
+	minAge := c.DefaultQuery("min_age", "0")
+	maxAge := c.DefaultQuery("max_age", "10")
+	education := c.Query("education")
+	maritalStatus := c.Query("marital_status")
+	occupation := c.Query("occupation")
+	income, _ := url.QueryUnescape(c.Query("income"))
 
-func FilterDataByMaritalStatus(c *gin.Context) {
-	FilterAndOrderData("marital_status", c)
-}
+	// Validación de campos permitidos
+	allowedFields := map[string]bool{
+		"education":      true,
+		"marital_status": true,
+		"occupation":     true,
+		"income":         true,
+	}
 
-func FilterDataByOccupation(c *gin.Context) {
-	FilterAndOrderData("occupation", c)
-}
-
-// filtrar y ordenar según un rango de edad
-func FilterDataByAgeRange(c *gin.Context) {
-	minAge := c.Query("min_age")
-	maxAge := c.Query("max_age")
+	// Validación para el rango de edad
 	if minAge == "" || maxAge == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing 'age' query parameter"})
 		return
 	}
-	orderBy := c.Query("order_by")
-	orderDirection := c.DefaultQuery("order_direction", "ASC")
-	if orderBy != "" && orderDirection != "ASC" && orderDirection != "DESC" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "'order_direction' must be 'ASC' or 'DESC'"})
+
+	// Validación filtrado de campos
+	if education != "" && !allowedFields["education"] {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid filter field: education"})
 		return
 	}
+	if maritalStatus != "" && !allowedFields["marital_status"] {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid filter field: marital_status"})
+		return
+	}
+	if occupation != "" && !allowedFields["occupation"] {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid filter field: occupation"})
+		return
+	}
+	if income != "" && !allowedFields["income"] {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid filter field: income"})
+		return
+	}
+
+	// construir la consulta
 	var filteredRecords []models.DataRecord
 	db := database.CreateDbConnection()
-
-	// aplicar filtro
-	query := db.Where("age BETWEEN ? AND ?", minAge, maxAge)
-
-	// Agregar ordenamiento si se especifica
-	if orderBy != "" {
-		query = query.Order(orderBy + " " + orderDirection)
-	}
-
-	// total de registros
-	var totalRecords int64
-	query.Model(&models.DataRecord{}).Count(&totalRecords)
-
-	query, page, pageSize := ApplyPagination(c, query)
-	tx := query.Find(&filteredRecords)
-	if tx.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to filter data by age"})
-		return
-	}
-
-	// responder con datos y metadatos de paginación
-	c.JSON(http.StatusOK, gin.H{
-		"data": filteredRecords,
-		"meta": gin.H{
-			"total_records": totalRecords,
-			"page":          page,
-			"page_size":     pageSize,
-			"total_pages":   (totalRecords + int64(pageSize) - 1) / int64(pageSize),
-		},
-	})
-}
-
-// filtrar y ordenar según los ingresos
-func FilterDataByIncome(c *gin.Context) {
-	income := c.Query("income")
-	income, err := url.QueryUnescape(income)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
-		return
-	}
-	if income == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing 'income' query parameter"})
-		return
-	}
-
-	orderBy := c.Query("order_by")
-	orderDirection := c.DefaultQuery("order_direction", "ASC")
-	if orderBy != "" && orderDirection != "ASC" && orderDirection != "DESC" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "'order_direction' must be 'ASC' or 'DESC'"})
-		return
-	}
-
-	var filteredRecords []models.DataRecord
-	db := database.CreateDbConnection()
-
 	query := db.Model(&models.DataRecord{})
 
-	// aplicar filtros según rango de ingresos
-	switch {
-	case strings.HasPrefix(income, "<"):
-		query = db.Where("income = ?", income)
-	case strings.HasPrefix(income, ">"):
-		query = db.Where("income = ?", income)
+	// Construir filtros
+	// Rango de edad
+	if minAge != "" && maxAge != "" {
+		query = db.Where("age BETWEEN ? AND ?", minAge, maxAge)
+	}
+	// educacion
+	if education != "" {
+		query = query.Where("education = ?", education)
+	}
+	// estado civil
+	if maritalStatus != "" {
+		query = query.Where("marital_status = ?", maritalStatus)
+	}
+	// ocupacion
+	if occupation != "" {
+		query = query.Where("occupation = ?", occupation)
+	}
+	// ingresos
+	if income != "" {
+		query = query.Where("income = ?", income) // Modifica según el formato de ingreso
 	}
 
-	// Agregar ordenamiento si se especifica
+	// Obtener el campo de ordenamiento y la dirección
+	orderBy := c.Query("order_by")
+	orderDirection := c.DefaultQuery("order_direction", "ASC")
 	if orderBy != "" {
 		query = query.Order(orderBy + " " + orderDirection)
 	}
 
+	// total de registros
 	var totalRecords int64
 	query.Model(&models.DataRecord{}).Count(&totalRecords)
 
+	// aplicar paginacion
 	query, page, pageSize := ApplyPagination(c, query)
+
+	// ejecutar la consulta final
+
 	tx := query.Find(&filteredRecords)
 	if tx.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to filter data by income range"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to filter data"})
 		return
 	}
 
 	// responder con datos y metadatos de paginación
 	c.JSON(http.StatusOK, gin.H{
 		"data": filteredRecords,
-		"meta": gin.H{
-			"total_records": totalRecords,
-			"page":          page,
-			"page_size":     pageSize,
-			"total_pages":   (totalRecords + int64(pageSize) - 1) / int64(pageSize),
-		},
-	})
-}
-
-// ordenar data en general según campo específico [sin filtrar]
-func OrderData(c *gin.Context) {
-	orderBy := c.Query("order_by")
-	orderDirection := c.DefaultQuery("order_direction", "ASC")
-	if orderBy == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing 'order_by' query parameter"})
-		return
-	}
-	if orderDirection != "ASC" && orderDirection != "DESC" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "'order_direction' must be 'ASC' or 'DESC'"})
-		return
-	}
-	var orderedRecords []models.DataRecord
-	db := database.CreateDbConnection()
-
-	// aplicar orden
-	query := db.Order(orderBy + " " + orderDirection)
-
-	// total de registros
-	var totalRecords int64
-	query.Model(&models.DataRecord{}).Count(&totalRecords)
-
-	// aplicar paginación
-	query, page, pageSize := ApplyPagination(c, query)
-	// obtener registros ordenados
-	tx := query.Find(&orderedRecords)
-	if tx.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to order data by ascendent"})
-		return
-	}
-
-	// responder con datos y metadatos de paginación
-	c.JSON(http.StatusOK, gin.H{
-		"data": orderedRecords,
 		"meta": gin.H{
 			"total_records": totalRecords,
 			"page":          page,
--- project_api/data/routes.go
+++ project_api/data/routes.go
@@ -8,11 +8,5 @@ func AddRoutes(router *gin.Engine) {
 	group := router.Group("/data")
 
 	group.GET("", ShowAllData)
-	group.GET("/filter/education", FilterDataByEducation)
-	group.GET("/filter/marital_status", FilterDataByMaritalStatus)
-	group.GET("/filter/occupation", FilterDataByOccupation)
-	group.GET("/filter/age", FilterDataByAgeRange)
-	group.GET("/filter/income", FilterDataByIncome)
-	group.GET("/order", OrderData)
-
+	group.GET("/filter", FilterAndOrderData)
 }
